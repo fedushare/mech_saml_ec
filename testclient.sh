ae3b14722a8b70b84fbcdcf7f69cee20e885c8db
Terrence Fleury <tfleury@illinois.edu>
2012-04-04 15:54:34 -0500

    Update README to use testserver.sh/testclient.sh. Changed testclient.sh to take username/password as parameters.

--- testclient.sh
+++ testclient.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH=/opt/moonshot/lib64 gss-sample/gss-client -nw -nx -nm -port 3490 -user tfleury -pass foobar -mech "{ 1 3 6 1 4 1 11591 4 6 }" localhost test testmessage
+if [ $# -ne 2 ] 
+then
+    echo "Usage: testclient.sh <username> <password>"
+    exit
+fi 
+
+LD_LIBRARY_PATH=/opt/moonshot/lib64 gss-sample/gss-client -nw -nx -nm -port 3490 -user "$1" -pass "$2" -mech "{ 1 3 6 1 4 1 11591 4 6 }" localhost test testmessage
