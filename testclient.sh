e523ab4bd8bd930a9c3dc60d00a7f67fc6d39055
Terrence Fleury <tfleury@illinois.edu>
2012-04-17 14:26:03 -0500

    Added SAML_EC_IDP='https://boingo...' environment variable to testserver and testclient startup scripts.

--- testclient.sh
+++ testclient.sh
@@ -6,4 +6,4 @@ then
     exit
 fi 
 
-LD_LIBRARY_PATH=/opt/moonshot/lib64 gss-sample/gss-client -nw -nx -nm -port 3490 -user "$1" -pass "$2" -mech "{ 1 3 6 1 4 1 11591 4 6 }" localhost test testmessage
+LD_LIBRARY_PATH=/opt/moonshot/lib64 SAML_EC_IDP='https://boingo.ncsa.uiuc.edu/idp/profile/SAML2/SOAP/ECP' gss-sample/gss-client -nw -nx -nm -port 3490 -user "$1" -pass "$2" -mech "{ 1 3 6 1 4 1 11591 4 6 }" localhost test testmessage
--- testserver.sh
+++ testserver.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH=/opt/moonshot/lib64 gss-sample/gss-server -port 3490 test
+LD_LIBRARY_PATH=/opt/moonshot/lib64 SAML_EC_IDP='https://boingo.ncsa.uiuc.edu/idp/profile/SAML2/SOAP/ECP' gss-sample/gss-server -port 3490 test
