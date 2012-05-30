af70162ed09888c3b72e3d02b620d41b05249786
Jim Basney <jbasney@illinois.edu>
2012-05-30 15:32:38 -0500

    a modified version of svn.shibboleth.net/java-idp-ecp/trunk/doc/testecp.sh for confirming ECP is working on an IdP

---
+++ testecp.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Simple test if an IdP's ECP endpoint is running
+
+# --------------------------------------------------------------------
+#
+# You need to set these parameters
+#
+#
+
+# userid = a valid login id on your system
+#userid="userid"
+userid=$LOGNAME
+
+# idphost = your idp hostname
+idphost="boingo.ncsa.uiuc.edu"
+
+# idpid = your idp entity id
+idpid="https://boingo.ncsa.uiuc.edu/shibboleth"
+
+# rpid = a valid SP entityId that is configured for ECP
+rpid="https://ec.ncsa.illinois.edu/shibboleth"
+
+# acsurl,ascurlbinding = an AssertionConsumerService URL and binding
+acsurl="https://ec.ncsa.illinois.edu/Shibboleth.sso/SAML2/ECP"
+acsurlbinding="urn:oasis:names:tc:SAML:2.0:bindings:PAOS"
+
+#
+# --------------------------------------------------------------------
+#
+
+# create a soap request
+
+now="`date -u +%Y-%m-%dT%H:%M:%S`"
+id="_`uuidgen | tr -d '-'`"
+# note. possible id if you dont have uuidgen: "_a7849c4e7188d592b1a266a34332ffbe"
+
+envelope='<S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/" xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"><S:Body><samlp:AuthnRequest xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" AssertionConsumerServiceURL="'${acsurl}'" ID="'${id}'" IssueInstant="'${now}'" ProtocolBinding="'${acsurlbinding}'" Version="2.0"><saml:Issuer xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion">'${rpid}'</saml:Issuer><samlp:NameIDPolicy AllowCreate="1"/><samlp:Scoping><samlp:IDPList><samlp:IDPEntry ProviderID="'${idpid}'"/></samlp:IDPList></samlp:Scoping></samlp:AuthnRequest></S:Body></S:Envelope>' 
+
+
+# make the request
+
+URL="https://${idphost}/idp/profile/SAML2/SOAP/ECP"
+resp="`curl -k -s -d \"$envelope\" -u \"${userid}\" \"$URL\" `" 
+
+# examine what we got
+
+echo "$resp" > testecp.out
+
+echo "$resp" | grep -q "Fault>"  
+[[ $? == 0 ]] && {
+   echo "ECP request failed: soap fault!"
+   echo "$resp"
+   exit 1
+}
+
+echo "$resp" | grep -q "Response"  
+[[ $? == 0 ]] && {
+   echo "ECP request successful!"
+   exit 0
+}
+
+echo "ECP request failed.  response:"
+echo "$resp"
+exit 1
+
