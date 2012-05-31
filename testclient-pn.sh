d21544f3836e8b9e38761cb1ad305dcd3291dc5d
Terrence Fleury <tfleury@illinois.edu>
2012-05-31 14:14:43 -0500

    Modified testclient script as follows: (1) can now specify user cert/key for client authn; (2) Added commented-out URL for ProtectNetwork to be used once encrypted assertions can be decrypted; (3) if user specifies both username and password on command line, the user cert/key values are ignored; (4) if using user cert/key, automatically append "SSL" to idp url.

--- testclient-pn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ $# -ne 2 ] 
-then
-    echo "Usage: testclient.sh <username> <password>"
-    exit
-fi 
-
-LD_LIBRARY_PATH=/opt/moonshot/lib64 SAML_EC_IDP='https://idp.protectnetwork.org/protectnetwork-idp/profile/SAML2/SOAP/ECP' gss-sample/gss-client -nw -nx -nm -port 3490 -user "$1" -pass "$2" -mech "{ 1 3 6 1 4 1 11591 4 6 }" localhost test testmessage
--- testclient.sh
+++ testclient.sh
@@ -2,20 +2,39 @@
 
 host="localhost"
 port=3490
+saml_ec_idp='https://boingo.ncsa.uiuc.edu/idp/profile/SAML2/SOAP/ECP'
+#saml_ec_idp='https://idp.protectnetwork.org/protectnetwork-idp/profile/SAML2/SOAP/ECP'
+saml_ec_user_cert=''
+saml_ec_user_key=''
 
-if [ $# -ne 1 ] 
+# If username and password passed via command line, ignore user cert/key
+if [ $# -eq 2 ] 
 then
-    read -p "Enter Username: " username
-else
-    username=$1
-fi 
+    saml_ec_user_cert=''
+    saml_ec_user_key=''
+fi
 
-if [ $# -ne 2 ] 
+# If user cert/key specified, do not prompt for username and password
+if [ ! -s "$saml_ec_user_cert" ] || [ ! -s "$saml_ec_user_key"]
 then
-    read -s -p "Enter Password: " password
-    echo
-else
-    password=$2
-fi 
+    if [ $# -lt 1 ] 
+    then
+        read -p "Enter Username: " username
+    else
+        username=$1
+    fi 
 
-LD_LIBRARY_PATH=/opt/moonshot/lib64 SAML_EC_IDP='https://boingo.ncsa.uiuc.edu/idp/profile/SAML2/SOAP/ECP' gss-sample/gss-client -nw -nx -nm -port $port -user "$username" -pass "$password" -mech "{ 1 3 6 1 4 1 11591 4 6 }" $host test testmessage
+    if [ $# -lt 2 ] 
+    then
+        read -s -p "Enter Password: " password
+        echo
+    else
+        password=$2
+    fi 
+else
+    # If using user cert/key, append 'SSL' to idp url
+    saml_ec_idp="$saml_ec_idp""SSL"
+fi
+
+LD_LIBRARY_PATH=/opt/moonshot/lib64 SAML_EC_IDP="$saml_ec_idp" SAML_EC_USER_CERT="$saml_ec_user_cert" SAML_EC_USER_KEY="$saml_ec_user_key" gss-sample/gss-client -nw -nx -nm -port $port -user "$username" -pass "$password" -mech "{ 1 3 6 1 4 1 11591 4 6 }" $host test testmessage
+
